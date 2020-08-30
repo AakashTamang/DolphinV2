ab95f8325c852c9b5d2a3408e2383b1d01e5e304
Shushant Pudasaini <shusrulz@gmail.com>
2020-08-30 15:26:05 +0545

    job parsing stanford model result included

--- dolphin/domain_classification/domain_classification.py
+++ dolphin/domain_classification/domain_classification.py
@@ -7,21 +7,21 @@ from collections import Counter
 
 
 
-
 domain_comparision_df = pd.read_csv('domain_class_data.tsv', sep="\t")
 domain_comparision_df['designations'] = domain_comparision_df['designations'].str.lower()
 
 
 finder = FinderAcora()
 sample_jd_text = prepare_text(
-    "/home/shushant/Desktop/Job Description Collection/Data Scientist 7.docx", dolower=False)
-
+    "/home/shushant/Desktop/Job Description Collection/Android Developer 2.docx", dolower=False)
+print(len(sample_jd_text))
 designations = finder.findall(sample_jd_text)
 designations_list = set([desig.match for desig in designations])
 desig = [desig.lower() for desig in designations_list]
-print(desig)
+print("Designation extracted -->", desig)
 required_df = domain_comparision_df.loc[domain_comparision_df['designations'].isin(
     desig)]
 domains = required_df['domain'].tolist()
-most_common_domain= [word for word, word_count in Counter(domains).most_common(1)]
-print(most_common_domain)
+most_common_domain = [word for word,
+                      word_count in Counter(domains).most_common(1)]
+print("Domain_classified-->", most_common_domain)
