f392bf727d136592aece936e802997f0b6225a48
Shushant Pudasaini <shusrulz@gmail.com>
2020-09-17 12:58:49 +0545

    duplicates from jd_parsing_module and no sentence response

--- dolphin/job_parsing/jd_parsing.py
+++ dolphin/job_parsing/jd_parsing.py
@@ -92,7 +92,7 @@ class SpacyNer():
         final_locations = [item for item in locations if len(item)<=50]
         final_designations = [item for item in designations if len(item)<=50]
         # ---Always return list of strings from here----
-        return final_designations, final_organizations, final_educations, final_experiences, final_locations
+        return list(set(final_designations)), list(set(final_organizations)), list(set(final_educations)), list(set(final_experiences)), list(set(final_locations))
 
     def get_skills(self,jd_content):
         '''
@@ -121,19 +121,21 @@ class SpacyNer():
         }
         return json_data
 
-if __name__ == "__main__":
-    # jd_path = r"C:\Users\Aakash\Desktop\Python\Scraping\Projects\jd_files\backend.txt")
-    # jd_content = prepare_text(jd_path, dolower=False)
-    # jd_content = "Qualifications Experience:US citizenship, 1 year (Required) Education : Bachelor's (Required) Work authorization : United States (Required) Full Job Description DOD client (US Citizenship required) Position Summary You will be a software engineer tasked with developing, testing and maintaining a suite of software applications and tools that support our aircraft survivability programs. Your role will require operating as a member of an integrated team alongside other engineering disciplines such as embedded software, systems engineering and product/test engineering. Basic Qualifications and Required Skills B.S. in Computer Science, Computer Engineering, Software Engineering or a related disciplne from an accredited institution or equivalent combination of education and experience.5+ years of experience in software engineering. Active security clearance or ability to obtain security clearance is required Expertise in Python Expertise in object oriented software development Experience developing desktop applications with the .NET Framework, C#, Visual Studio Experience developing desktop applications and/or software libraries with C or C++ is nice to haveExperience with source control repositories such as Git or SVNExperience following an established software engineering processExperience with software build tools such as JenkinsExcellent written and verbal communication skills required Duties and Responsibilities Responsible for software engineering tasks including requirements/user story definition, software application and library design, implementation, integration, verification, validation and maintenance. Other duties as assigned.Job Type: ContractPay: $50.00 - $70.00 per hour Benefits: Dental insurance Health insurance Schedule: Monday to Friday COVID-19 considerations:Due to the nature of the work, candidates must be available to work onsite in a secure area. Experience : US citizenship: 1 year (Required) Education :Bachelor's (Required) Work authorization: United States (Required) Contract Renewal: Likely Full Time Opportunity : YesCompany's website:www.cornerstonetek.com Work Remotely:Temporarily due to COVID-19 If you require alternative methods of application or screening, you must approach the employer directly to request this as Indeed is not responsible for the employer's application process.\n"
-    # spacy_obj = SpacyNer()
-    # designations, organization, experience, education, location = spacy_obj.parse(
-    #     jd_content)
-    # print("Organizations -->", organization)
-    # print("Locations --->", location)
-    # print("Designation ---->", designations)
-    # print("Education --->", education)
-    # print("Experience ---> ", experience)
+# if __name__ == "__main__":
+#     # jd_path = r"C:\Users\Aakash\Desktop\Python\Scraping\Projects\jd_files\backend.txt"))
+#     # jd_content = prepare_text(jd_path, dolower=False)
+
+#     jd_content = "Job Responsibilities:Implements high priority application software and infrastructure covering database design, epic, feature and story development, re-usable code, components and application functionality.Works with senior engineers and architects to define the application architecture and create software design for key elements of the application.Evaluates performance of key elements of the application functionality and tunes the performance to cover the range of customer use.Works with the scrum team to assist developers and monitor their progress against project milestones. Performs tech design reviews and code reviews for the scrum teams. Provides scheduling estimates and assists with the scheduling process.Provides input to managers on the performance of team members for use in their reviews and participates in the interview process for new candidates.Preferred Education, Experience, and Skills:Education: Bachelor's degree or comparable work experience in software development.Experience: at least 5-8 years of experience in software development in addition to education requirementsMust be proficient with: Java, .net, powershell, SQLServer, T-SQL, Javascript, CSS, JQuery, React.Willing to experience building high throughput and scalable data pipelines using Hadoop/ Cloudera (HBase, Hive, Storm, Druid, Oozi)Proven track record with planning and delivering coding assignments with high quality.Motivated, self-starter results-oriented team player willing to do “whatever it takes” in a dynamic and stimulating environment.Great verbal and written communication skillsOversee and/or lead analysis, architecture design, development and maintenance of large scale and/or complex applicationsWrite modular, reusable and excellent quality code, code reviews, and implement best practicesSupport the migration and redeployment of existing application (if any)Take responsibility of project and ensure robust hosting, application stability, security and system integrationTechnical Skills Preferred:Java, Java Applets, Web Programming Skills, Teamwork, Verbal Communication, Web User Interface Design, Software Requirements, Software Development Process, Object-Oriented Design (OOD), Multimedia Content Development, Software Debugging\n"
+#     # jd_content = "Qualifications Experience:US citizenship, 1 year (Required) Education : Bachelor's (Required) Work authorization : United States (Required) Full Job Description DOD client (US Citizenship required) Position Summary You will be a software engineer tasked with developing, testing and maintaining a suite of software applications and tools that support our aircraft survivability programs. Your role will require operating as a member of an integrated team alongside other engineering disciplines such as embedded software, systems engineering and product/test engineering. Basic Qualifications and Required Skills B.S. in Computer Science, Computer Engineering, Software Engineering or a related disciplne from an accredited institution or equivalent combination of education and experience.5+ years of experience in software engineering. Active security clearance or ability to obtain security clearance is required Expertise in Python Expertise in object oriented software development Experience developing desktop applications with the .NET Framework, C#, Visual Studio Experience developing desktop applications and/or software libraries with C or C++ is nice to haveExperience with source control repositories such as Git or SVNExperience following an established software engineering processExperience with software build tools such as JenkinsExcellent written and verbal communication skills required Duties and Responsibilities Responsible for software engineering tasks including requirements/user story definition, software application and library design, implementation, integration, verification, validation and maintenance. Other duties as assigned.Job Type: ContractPay: $50.00 - $70.00 per hour Benefits: Dental insurance Health insurance Schedule: Monday to Friday COVID-19 considerations:Due to the nature of the work, candidates must be available to work onsite in a secure area. Experience : US citizenship: 1 year (Required) Education :Bachelor's (Required) Work authorization: United States (Required) Contract Renewal: Likely Full Time Opportunity : YesCompany's website:www.cornerstonetek.com Work Remotely:Temporarily due to COVID-19 If you require alternative methods of application or screening, you must approach the employer directly to request this as Indeed is not responsible for the employer's application process.\n"
+#     spacy_obj = SpacyNer()
+#     designations, organization, experience, education, location = spacy_obj.parse(
+#         jd_content)
+#     print("Organizations -->", organization)
+#     print("Locations --->", location)
+#     print("Designation ---->", designations)
+#     print("Education --->", education)
+#     print("Experience ---> ", experience)
     
-    # technical_skills, soft_skills = spacy_obj.get_skills(jd_content)
-    # print (f"Technical Skills: {technical_skills}")
-    # print (f"Soft Skills: {soft_skills}")
+#     technical_skills, soft_skills = spacy_obj.get_skills(jd_content)
+#     print (f"Technical Skills: {technical_skills}")
+#     print (f"Soft Skills: {soft_skills}")
